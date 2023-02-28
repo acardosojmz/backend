e4c3a48d239ed94b61e355f0a060a6f4153c8626
acardosojmz <ambrosio.cj@voaxaca.tecnm.mx>
2023-02-28 12:42:20 -0600

    cambios menores para validación de nulos

--- src/main/kotlin/abstractfactory/TestAbstractFactory.kt
+++ src/main/kotlin/abstractfactory/TestAbstractFactory.kt
@@ -4,10 +4,10 @@ package abstractfactory
 
 fun main(){
     val factory: IServiceStackAbstractFactory? = ServiceStackAbstractFactory.createServiceFactory()
-    val employeeService = factory!!.getEmployeeService()
-    val productService = factory!!.getProductsService()
-    println("EmployeeService class > " + employeeService.javaClass.canonicalName)
-    println("ProductService class > " + productService.javaClass.canonicalName)
-    println("getEmployee > " + employeeService.getEmployee())
-    println("getProduct > " + productService.getProducts())
+    val employeeService = factory?.getEmployeeService()
+    val productService = factory?.getProductsService()
+    println("EmployeeService class > " + employeeService?.javaClass?.canonicalName)
+    println("ProductService class > " + productService?.javaClass?.canonicalName)
+    println("getEmployee > " + employeeService?.getEmployee())
+    println("getProduct > " + productService?.getProducts())
 }
